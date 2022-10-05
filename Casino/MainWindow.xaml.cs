9eab578cf77e9982bf0c369bb29d4d6e5b2b92b5
201913 <201913@1220-008-22>
2022-10-05 16:03:24 +0300

    Add project files.

---
+++ Casino/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Casino
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            frame.NavigationService.Navigate(new Pages.LoginPage());
+        }
+        private void btnGoBack_Click(object sender, RoutedEventArgs e)
+        {
+            if (frame.NavigationService.CanGoBack)
+                frame.NavigationService.GoBack();
+        }
+
+        private void btnGoForward_Click(object sender, RoutedEventArgs e)
+        {
+            if (frame.NavigationService.CanGoForward)
+                frame.NavigationService.GoForward();
+        }
+        private void FrameNavigated(object sender, NavigationEventArgs e)
+        {
+            if (frame.Content is Pages.LoginPage)
+            {
+                btnGoForward.Visibility = Visibility.Hidden;
+                btnGoBack.Visibility = Visibility.Hidden;
+            }
+            else if (frame.Content is Pages.RegistrationPage)
+            {
+                btnGoForward.Visibility = Visibility.Hidden;
+                btnGoBack.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                btnGoBack.Visibility = Visibility.Visible;
+                btnGoForward.Visibility = Visibility.Visible;
+            }
+        }
+    }
+}
---
+++ Casino/Pages/RegistrationPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Casino.Pages
+{
+    /// <summary>
+    /// Interaction logic for RegistrationPage.xaml
+    /// </summary>
+    public partial class RegistrationPage : Page
+    {
+        public RegistrationPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRegistration_Click(object sender, RoutedEventArgs e)
+        {
+            var login = tbLogin.Text;
+            var passwrod = pbPassword.Password;
+        }
+    }
+}
